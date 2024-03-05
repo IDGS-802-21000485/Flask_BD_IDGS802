fccb5810144c5bf09a7f6ffaac61d1485bc51245
IDGS-802-21000485 <156386746+IDGS-802-21000485@users.noreply.github.com>
2024-03-04 19:52:28 -0600

    AFBF_29/02/2024_ABCompleto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Response
+from flask import Flask, request, render_template, Response, redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from config import DevelomentConfig
@@ -31,10 +31,53 @@ def index():
     return render_template("index.html", form=alumnos_form)
 
 @app.route("/ABC_alumnos",  methods=("GET", "POST"))
-def ABCProfe():
+def ABCAlumno():
     profe_form = forms.UserForm(request.form)
     alumnos = alumno.query.all()
     return render_template('ABC_Alumnos.html', alumno=alumnos)
+
+@app.route("/eliminar", methods=("GET", "POST"))
+def eliminar():
+
+    alum_form = forms.UserForm(request.form)
+    if request.method == "GET":
+        id = request.args.get('id')
+        alum1 = db.session.query(alumno).filter(alumno.id==id).first()
+        alum_form.id.data = request.args.get('id')
+        alum_form.nombre.data = alum1.nombre
+        alum_form.apaterno.data = alum1.apaterno
+        alum_form.email.data = alum1.correo
+    if request.method == "POST":
+        id=alum_form.id.data
+        alum=alumno.query.get(id)
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('ABCAlumno'))
+    return render_template('eliminar.html', form = alum_form)
+
+@app.route("/modificar", methods=("GET", "POST"))
+def modificar():
+
+    alum_form = forms.UserForm(request.form)
+    if request.method == "GET":
+        id = request.args.get('id')
+        alum1 = db.session.query(alumno).filter(alumno.id==id).first()
+        alum_form.id.data = request.args.get('id')
+        alum_form.nombre.data = alum1.nombre
+        alum_form.apaterno.data = alum1.apaterno
+        alum_form.email.data = alum1.correo
+    if request.method == "POST":
+        id=alum_form.id.data
+        
+        alum = db.session.query(alumno).filter(alumno.id == id).first()
+        alum.nombre = alum_form.nombre
+        alum.apaterno = alum_form.apaterno
+        alum.correo = alum_form.email
+        alum=alumno.query.get(id)
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('ABCAlumno'))
+    return render_template('eliminar.html', form = alum_form)
 
 @app.route("/alumnos", methods=("GET", "POST"))
 def alumnos():
